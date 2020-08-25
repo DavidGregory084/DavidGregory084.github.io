24d1b013a980bd9eb3700155706a6608a43ec4f1
David Gregory <david.gregory@opencastsoftware.com>
2020-08-25 20:43:23 +0100

    Add copyright footer, add lorem ipsum to sample post

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,27 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const rss = require("@11ty/eleventy-plugin-rss");
+const moment = require("moment");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPlugin(rss);
+
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("images");
+
     eleventyConfig.setDataDeepMerge(true);
+
+    eleventyConfig.addShortcode('copyrightYear', function() {
+        const currentDate = moment.utc();
+
+        const copyrightYear =
+            currentDate.year() == 2020 ?
+            "2020" :
+            `2020 - ${currentDate.format('YYYY')}`;
+
+        return `Copyright &#169; ${copyrightYear} David Gregory`;
+    })
+
     return {
         dir: {
             layouts: "_layouts",
