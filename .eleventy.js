f7212e472fb6c6cc0d7e150b16e536719117cd1e
David Gregory <david.gregory@opencastsoftware.com>
2020-08-27 17:23:21 +0100

    Vendor fonts and CSS because unpkg is really slow

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("vendor");
 
     eleventyConfig.setDataDeepMerge(true);
 
